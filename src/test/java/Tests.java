5d9fcb871e7bc56d329bfbf98ab90263c1bd3cb8
Basheer Jarrah <78933834+JARRAH2000@users.noreply.github.com>
2024-02-18 18:13:47 +0300

    first-commit-selenium-azure-devops

---
+++ src/test/java/Tests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tests {
+    WebDriver webDriver;
+
+    @Test
+    public void firstTest() {
+        webDriver = new ChromeDriver();
+
+        webDriver.navigate().to("https://www.google.com/");
+    }
+}
